ea200a488ec15fda3fc5b81ae360e8f94cc66ab9
amsesk <amsesk@umich.edu>
2023-03-06 09:13:17 -0800

    fixes

--- setup_instance/setup.bash
+++ setup_instance/setup.bash
@@ -5,29 +5,22 @@ mkdir -p $HOME/pkgs
 
 # Install ubuntu packages - many of these required by R packages
 sudo apt update
-sudo apt install -y r-base-core libhdf5-dev tmux libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libxml2-dev zsh
-
-#Change shell to zsh
-sudo chsh ubuntu /usr/bin/zsh
-rm $HOME/.zshrc
-ln -s $HOME/microbioinfo-aws/setup_instance/config/zshrc $HOME/.zshrc
+sudo apt install -y r-base-core libhdf5-dev tmux libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libxml2-dev
 
 # Setup neovim
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb -P $HOME/pkgs/.
 sudo apt install $HOME/pkgs/nvim-linux64.deb
 sudo rm /usr/bin/vim
-ln -s /usr/bin/nvim /usr/bin/vim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\\n $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+sudo ln -s /usr/bin/nvim /usr/bin/vim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/penn-microbioinfo/microbioinfo-aws.git $HOME/.
 mkdir -p $HOME/.config
-ln -s $HOME/microbioinfo-aws/setup_instance/config/nvim $HOME/.config/nvim
+ln -s $HOME/microbioinfo-aws/setup_instance/config/nvim $HOME/.config/
 vim "+PackerInstall" "+q" tmp
 
 # Create the expected R_USER_LIBS directory so that 
 r_user_libs_path=$(cat /etc/R/Renviron | grep R_LIBS_USER | grep -v '^#' | grep -Eo "['][^']+[']" | sed "s/'//g")
-mkdir -p $r_user_libs_path
+mkdir -p $(echo $r_user_libs_path | sed 's#~#'$HOME'#')
 
 # Install Seurat, tidyverse, and other R dependencies
 Rscript install_R_deps.R
-
-echo "Shell has been changes to zsh and config copied to ~/.zshrc - restart session for changes to take effect."
