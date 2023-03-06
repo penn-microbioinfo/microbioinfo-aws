60eba55f6ea27bc442a71b73da982f619b8c625a
amsesk <amsesk@umich.edu>
2023-03-06 08:43:47 -0800

    initial

--- setup_instance/setup.bash
+++ setup_instance/setup.bash
@@ -5,7 +5,12 @@ mkdir -p $HOME/pkgs
 
 # Install ubuntu packages - many of these required by R packages
 sudo apt update
-sudo apt install -y r-base-core libhdf5-dev tmux libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libxml2-dev
+sudo apt install -y r-base-core libhdf5-dev tmux libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libxml2-dev zsh
+
+#Change shell to zsh
+sudo chsh ubuntu /usr/bin/zsh
+rm $HOME/.zshrc
+ln -s $HOME/microbioinfo-aws/setup_instance/config/zshrc $HOME/.zshrc
 
 # Setup neovim
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb -P $HOME/pkgs/.
@@ -16,7 +21,7 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\\n $HOME/.local/sh
 git clone https://github.com/penn-microbioinfo/microbioinfo-aws.git $HOME/.
 mkdir -p $HOME/.config
 ln -s $HOME/microbioinfo-aws/setup_instance/config/nvim $HOME/.config/nvim
-vim tmp -c PackerInstall -c q
+vim "+PackerInstall" "+q" tmp
 
 # Create the expected R_USER_LIBS directory so that 
 r_user_libs_path=$(cat /etc/R/Renviron | grep R_LIBS_USER | grep -v '^#' | grep -Eo "['][^']+[']" | sed "s/'//g")
@@ -24,3 +29,5 @@ mkdir -p $r_user_libs_path
 
 # Install Seurat, tidyverse, and other R dependencies
 Rscript install_R_deps.R
+
+echo "Shell has been changes to zsh and config copied to ~/.zshrc - restart session for changes to take effect."
