85c9e96c836755470d8e1722b020d589fd7f8608
amsesk <amsesk@umich.edu>
2023-03-07 03:14:10 -0800

    export env var

--- setup_instance/setup_fed_auth.py
+++ setup_instance/setup_fed_auth.py
@@ -28,6 +28,6 @@ python3 setup.py develop
 
 cd ../
 
-echo "AWS_PROFILE=445654575720-BushmanLabPostdoc" >> $HOME/.zshrc
+echo "export AWS_PROFILE=445654575720-BushmanLabPostdoc" >> $HOME/.zshrc
 echo "alias awsenv='source $HOME/pyenvs/pennaws/bin/activate'" >> $HOME/.zshrc
 echo "alias awsauth='awsenv; python ${HOME}/lib/aws-federated-auth/aws-federated-auth.py'" >> $HOME/.zshrc
