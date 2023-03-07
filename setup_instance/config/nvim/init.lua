d0e343071f9063e023833d30e941bbaf75f768c8
amsesk <amsesk@umich.edu>
2023-03-07 02:04:25 -0800

    try impl try_require

--- setup_instance/config/nvim/init.lua
+++ setup_instance/config/nvim/init.lua
@@ -14,15 +14,15 @@ end
 -- Packer plugins
 require('plugins').install()
 
-require'lualine'.setup {
+try_require'lualine'.setup {
     options = { theme = require'lualine.themes.horizon' },
 }
-require'nvim-treesitter.configs'.setup {
+try_require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
   ensure_installed = { "bash", "c", "lua", "vim", "help", "python", "r", "rust" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
