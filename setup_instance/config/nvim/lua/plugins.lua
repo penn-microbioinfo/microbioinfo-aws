65db8a91ded2ab47438aa49d57b02fb478bc797d
amsesk <amsesk@umich.edu>
2023-03-07 05:21:27 -0800

    add nvim-R

--- setup_instance/config/nvim/lua/plugins.lua
+++ setup_instance/config/nvim/lua/plugins.lua
@@ -1,16 +1,11 @@
 --vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
-    use { 
-        'wbthomason/packer.nvim' 
-    }
+    use 'wbthomason/packer.nvim' 
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
-    use {
-        'nvim-treesitter/nvim-treesitter'
-    }
-    use {
-        'glepnir/zephyr-nvim'
-    }
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'glepnir/zephyr-nvim'
+    use 'jalvesaq/Nvim-R'
 end)
