452d4681441ee18d675a24654388f406d6238110
amsesk <amsesk@umich.edu>
2023-03-07 01:46:09 -0800

    try impl try_require

--- setup_instance/config/nvim/init.lua
+++ setup_instance/config/nvim/init.lua
@@ -17,7 +17,7 @@ require('plugins').install()
 try_require'lualine'.setup {
     options = { theme = require'lualine.themes.horizon' },
 }
-require'nvim-treesitter.configs'.setup {
+try_require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
   ensure_installed = { "bash", "c", "lua", "vim", "help", "python", "r", "rust" },
 
