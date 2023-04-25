c82620a257c464321e9063ff489a879a321d4ad6
Ubuntu <ubuntu@ip-10-128-254-50.ec2.internal>
2023-04-25 23:31:13 +0000

    add a very important list.sort()

--- scripts/s3/s3-multipart-upload.py
+++ scripts/s3/s3-multipart-upload.py
@@ -1,4 +1,5 @@
 import boto3
+import io
 import botocore
 import json
 import argparse
@@ -9,6 +10,7 @@ import re
 import sys
 import hashlib
 import base64
+import time
 
 PARTS_FNAME_PATTERN="^x[a-z]{2}$"
 
@@ -27,17 +29,19 @@ class S3MultiPartUpload(object):
 
     def _create_upload(self):
         return self.client.create_multipart_upload(Bucket = self.bucket, Key = self.key)["UploadId"]
-    
     def _upload_part(self, partnumber, partpath, parthash):
+        start = time.time()
         with open(partpath, 'rb') as fopen:
             try:
                 self.client.upload_part(Bucket = self.bucket, Key = self.key, UploadId = self.upload_id, PartNumber = partnumber, Body = fopen, ContentMD5 = parthash)
             except botocore.exceptions.ClientError:
                 self._abort()
                 raise
+        logging.info(f"Uploaded part #{partnumber}: {partpath} - {time.time()-start} seconds")
 
     def upload_parts(self, part_fnames):
         self.part_fnames = part_fnames
+        logging.info(f"Part filenames (IN THIS ORDER): {self.part_fnames}")
         self.part_digests = S3MultiPartUpload._md5_digest_parts(self.part_fnames)
         for i,f,d in zip(range(1, len(self.part_fnames)+1), self.part_fnames, S3MultiPartUpload._encode_b64(self.part_digests)
 ):
@@ -45,7 +49,7 @@ class S3MultiPartUpload(object):
     
     def _generate_etag (self):
         if self.part_fnames is not None and self.part_digests is not None:
-            self.etag =  f"{md5hash(b''.join(self.part_digests)).hexdigest()}-{len(self.part_digests)}"
+            self.etag =  f"{md5hash( io.BytesIO(b''.join(self.part_digests)) ).hexdigest()}-{len(self.part_digests)}"
         else:
             raise ValueError("Cannot generate ETag without parts.")
     
@@ -59,7 +63,7 @@ class S3MultiPartUpload(object):
         return [base64.b64encode(x).decode("utf-8") for x in part_digests]
     
     def _md5_digest_parts(part_fnames):
-        return [md5hash(open(x, 'rb').read()).digest() for x in part_fnames]
+        return [md5hash(open(x, 'rb')).digest() for x in part_fnames]
 
     def list_parts(self):
         try:
@@ -104,15 +108,19 @@ def get_upload_id(response):
 
 def make_file_parts(largefile, part_size = 4950000000, parts_outdir = None): 
     cmd = ["split", "-b", str(part_size), largefile] 
+    p = subprocess.run(cmd, capture_output = True)
     try:
-        p = subprocess.run(cmd, capture_output = True, check=True)
+        p.check_returncode()
     except subprocess.CalledProcessError:
-        out, err = p.communicate()
-        logging.critical(f"Call to `split` failed with: {err}")
+        logging.critical(f"Call to `split` failed with: {p.stderr}")
         sys.exit(1)
 
     pat = re.compile(PARTS_FNAME_PATTERN)
     part_fnames = [x for x in os.listdir(".") if re.match(pat, x)]
+    
+    # This sort is very important, becasue python reads the file names out
+    # of sort order
+    part_fnames.sort()
     
     check_file_part_sizes(largefile, part_fnames)
 
@@ -126,10 +134,14 @@ def check_file_part_sizes(largefile, part_fnames):
         logging.critical(f"Sum of file part sizes is not equal to file size. {file_parts_total_size} != {largefile_size}")
         sys.exit(1)
 
-def md5hash(bytestream):
-    hl = hashlib.new("md5")
-    hl.update(bytestream)
-    return hl
+def md5hash(f, block_size = 2**20):
+    md5 = hashlib.new("md5")
+    while True:
+        block = f.read(block_size)
+        if not block:
+            break
+        md5.update(block)
+    return md5
 
 
 def check_final_etag(local_etag, remote_etag):
@@ -138,6 +150,7 @@ def check_final_etag(local_etag, remote_etag):
         sys.exit(1)
 
 if __name__ == "__main__":
+    logging.basicConfig(encoding='utf-8', level=logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--bucket", action = "store", help = "")
     parser.add_argument("-k", "--key", action = "store", help = "")
