387a6842e5fa47cf1c06588dce2d1676ac239ae5
Ubuntu <ubuntu@ip-10-128-254-59.ec2.internal>
2023-05-01 22:50:16 +0000

    add additional checking for checksums and generate whole-file md5

--- scripts/s3/s3-multipart-upload.py
+++ scripts/s3/s3-multipart-upload.py
@@ -19,6 +19,18 @@ PARTS_FNAME_PATTERN="^x[a-z]{2}$"
 def pooled(f, ):
     f()
     
+class Part(object):
+    def __init__(self, partnumber, part_fname, part_digest = None, part_hexdigest = None):
+        self.number = partnumber
+        self.fname = part_fname
+        self.digest = part_digest
+        self.digest = part_hexdigest
+    def _etag(self):
+        if self.digest is not None:
+            return md5hash(io.BytesIO(self.digest)).hexdigest()
+            #self.etag =  f"{md5hash( io.BytesIO(b''.join(self.part_digests)) ).hexdigest()}-{len(self.part_digests)}"
+        else:
+            raise ValueError("No part digest to make etag out of.")
 
 class S3MultiPartUpload(object):
     def __init__(self, bucket, key, abort_on_failure_to_complete = False, nproc = 1):
@@ -26,10 +38,10 @@ class S3MultiPartUpload(object):
         self.bucket = bucket
         self.key = key
         self.upload_id = None
-        self.part_fnames = None
+        #self.part_fnames = None
         self.parts = None
         self.nparts = None
-        self.part_digests = None
+        #self.part_digests = None
         self.etag = None
         self.abort_on_failure_to_complete = abort_on_failure_to_complete
         self.nproc = nproc
@@ -48,37 +60,48 @@ class S3MultiPartUpload(object):
                 self._abort()
                 raise
         logging.info(f"Uploaded part #{partnumber}: {partpath} - {time.time()-start} seconds")
+
+    def _part_fnames(self):
+        return [p.fname for p in self.parts]
     
+    def _part_numbers(self):
+        return [p.number for p in self.parts]
+
+    def _part_digests(self, as_bytes = True):
+        if as_bytes:
+            return [p.digest for p in self.parts]
+        else:
+            return [p.hexdigest for p in self.parts]
+
     def upload_parts(self, part_fnames):
-        self.part_fnames = part_fnames
         self.nparts = len(part_fnames)
-        self.parts = {pn: pf for pn,pf in zip(range(1,self.nparts+1), self.part_fnames)}
+        self.parts = [Part(pn, pf) for pn,pf in zip(range(1,self.nparts+1), part_fnames)]
         if self.nparts < self.nproc:
             logging.info(f"Only using as many processes as there are parts: {self.nparts}")
             self.nproc = self.nparts
-        logging.info(f"Part filenames (IN THIS ORDER): {self.part_fnames}")
-        print(self.parts)
-        self.part_digests = S3MultiPartUpload._md5_digest_parts(self.part_fnames, self.nproc)
-        #print('\n'.join([str(x) for x in self.part_digests]))
+        logging.info(f"Part filenames (IN THIS ORDER): {self._part_fnames()}")
+        self.part_digests = S3MultiPartUpload._md5_digest_parts(self.parts, self.nproc)
 
-        #parts = zip(range(1, len(self.part_fnames)+1), self.part_fnames, S3MultiPartUpload._encode_b64(self.part_digests))
         logging.info(f"Started uploading {self.nparts} parts.")
         start = time.time()
         with ProcessPool(nodes=self.nproc) as p:
-            p.map(self._upload_part, self.parts.keys(), self.parts.values(), S3MultiPartUpload._encode_b64(self.part_digests))
+            p.map(self._upload_part, self._part_numbers(), self._part_fnames(), S3MultiPartUpload._encode_b64([p.digest for p in self.parts]))
    
         logging.info(f"Total upload time ({self.nparts} parts): {time.time()-start} seconds")
 
         self.delete_part_files()
 
     def delete_part_files(self):
-        for pf in self.part_fnames:
+        for pf in self._part_fnames():
             os.remove(pf)
-        logging.info(f"Removed {len(self.parts)} part files: {self.part_fnames})")
+        logging.info(f"Removed {len(self.parts)} part files: {self._part_fnames()})")
 
-    def _generate_etag (self):
-        if self.part_fnames is not None and self.part_digests is not None:
-            self.etag =  f"{md5hash( io.BytesIO(b''.join(self.part_digests)) ).hexdigest()}-{len(self.part_digests)}"
+    def _etag():
+        pass
+
+    def _generate_final_etag (self):
+        if self.parts is not None and all([p.digest is not None for p in self.parts]):
+            self.etag =  f"{md5hash( io.BytesIO(b''.join(self._part_digests())) ).hexdigest()}-{len(self._part_digests())}"
         else:
             raise ValueError("Cannot generate ETag without parts.")
     
@@ -91,13 +114,22 @@ class S3MultiPartUpload(object):
     def _encode_b64(part_digests):
         return [base64.b64encode(x).decode("utf-8") for x in part_digests]
     
-    def _md5_digest_parts(part_fnames, local_nproc = 1):
+    def _md5_digest_parts(parts, local_nproc = 1):
+        def both_digests(x):
+            x_hash = md5hash(x)
+            x_digest = x_hash.digest()
+            x_hexdigest = x_hash.hexdigest()
+            return (x_digest, x_hexdigest)
+
         logging.info(f"Generating md5 digests for {len(part_fnames)} file parts.")
-        streams = [open(x, 'rb') for x in part_fnames]
+        streams = [open(x, 'rb') for x in [p.fname for p in parts]]
         with ProcessPool(local_nproc) as p:
-            digests = p.map(lambda x: md5hash(x).digest(), streams)
+            digests = p.map(lambda x: both_digests(x), streams)
         close_streams = [x.close() for x in streams]
-        return digests
+        for i,p in enumerate(parts):
+            p.digest = digests[i][0]
+            p.hexdigest = digests[i][1]
+        return [d[0] for d in digests]
         
         #return [md5hash(open(x, 'rb')).digest() for x in part_fnames]
 
@@ -109,8 +141,32 @@ class S3MultiPartUpload(object):
             raise
         return r["Parts"]
 
+    def _uploaded_parts_in_correct_order(self):
+        # Sort remote parts by part number
+        remote_parts = {p["PartNumber"]: p["ETag"].replace('"', '') for p in self.list_parts()}
+        remote_parts = {i: remote_parts[i] for i in sorted(remote_parts.keys())}
+        
+        # Sort local parts by filename
+        local_parts = {p.fname: (p.number, p.hexdigest) for p in self.parts} 
+        local_parts = {i: local_parts[i] for i in sorted(local_parts.keys())} 
+      
+        # To make sure that the remote parts are numbered correctly, compare the hexdigests
+        # from each list (sorted differently)
+        local_fnames = list(local_parts.keys())
+        remote_etags = list(remote_parts.values())
+        local_etags = [x[1] for x in local_parts.values()]
+        for idx,local in enumerate(local_etags):
+            logging.info(f"{local_fnames[idx]}\t{local}\t{remote_etags[idx]}")
+            if local != remote_etags[idx]:
+                return False 
+        return True
+        return True
     def complete_upload(self):
-        self._generate_etag()
+        
+        if not self._uploaded_parts_in_correct_order():
+            raise ValueError("Local and remote parts mismatched.")
+
+        self._generate_final_etag()
 
         #check list_parts against stored parts()
 
@@ -195,10 +251,20 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--nproc", action = "store", default = cpu_count(), type = int, help = "")
     args = parser.parse_args()
 
-    logging.info(f"Uploading with up to {args.nproc} processes (default = detected CPUS ({cpu_count()}))")
+    logging.info(f"Uploading with up to {args.nproc} processes (default = detected {cpu_count()} CPUs))")
 
+    logging.info(f"Generating MD5 for {args.largefile} in background.")
     logging.info(f"Splitting {args.largefile} ({float(os.stat(args.largefile).st_size)/float(1e9)} Gb) into parts. ")
+
+    md5p = subprocess.Popen(["md5sum", args.largefile], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     part_fnames = make_file_parts(args.largefile, part_size = args.partsize)
+    out,err = md5p.communicate()
+    if md5p.returncode != 0:
+        raise OSError(err)
+    else:
+        with open(f"{args.largefile}.md5", 'wb') as largefile_md5:
+            largefile_md5.write(out)
+            
 
     # Begin interactions with S3
     mpu = S3MultiPartUpload(args.bucket, args.key, nproc = args.nproc)
