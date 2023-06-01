351eb79842bc1e450d09af935009b439df4f44d4
AWS ParallelCluster user <ec2-user@ip-10-0-0-128.ec2.internal>
2023-06-01 18:34:28 +0000

    expand compatibility

--- scripts/jobgen/make_cellranger_table.py
+++ scripts/jobgen/make_cellranger_table.py
@@ -29,6 +29,26 @@ def object_read_number(pattern, objkey):
     else:
         raise ValueError(f"No matching read number: {objkey}")
 
+# This function defaults to assuming that the protocol type is present in the second underscore-separated
+# column of the file name, which should be the case if using `make_sample_sheet.bash` 
+def object_protocol_type(fname, sep = "_", pos = 1, protocol_converter = None):
+    protocol = fname.split(sep)[pos]
+    if protocol_converter is not None:
+        assert isinstance(protocol_converter, dict), "protocol_converter passed to object_protocol_type needs to be type dict"
+        try:
+            return protocol_converter[protocol]
+        except KeyError:
+            raise
+    else:
+        return protocol
+
+emtab_style_protocol_converter = {
+        "RNA": "scRNA-seq",
+        "VDJ": "scVDJ-seq",
+        "ADT": "scADT-seq",
+        "snRNA-seq": "scADT-seq"
+        }
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--bucket", required = True)
 parser.add_argument("--prefix", required = True)
@@ -36,8 +56,8 @@ parser.add_argument("--pattern", required = True)
 args = parser.parse_args()
 
 p = re.compile(args.pattern)
-read_num_pat=re.compile("[_](R[0-9])[_]")
+read_num_pat=re.compile("[_](R[0-9])[_]"
 for k in s3_get_object_keys(args.bucket, args.prefix):
     if object_key_matches(p, k):
         key_parts = os.path.split(k)
-        print("\t".join(["scRNA-seq", key_parts[1], object_read_number(read_num_pat,k), key_parts[0]]))
+        print("\t".join([object_protocol_type(key_parts[1], protocol_converter = emtab_style_protocol_converter), key_parts[1], object_read_number(read_num_pat,k), key_parts[0]]))
