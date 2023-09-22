61020cb92d5cce9bd9b81a67707dd3a26ba18d6a
Ubuntu <ubuntu@ip-10-128-254-44.ec2.internal>
2023-09-22 18:44:56 +0000

    change to stable

--- setup_instance/neovim_setup.bash
+++ setup_instance/neovim_setup.bash
@@ -10,18 +10,20 @@ if [ ! -d $HOME/pkgs ]; then
 	mkdir $HOME/pkgs
 fi
 
-# Install node.js if needed
+ Install node.js if needed
 if ! which node; then
     curl -sL install-node.vercel.app/lts | sudo bash # Install node.js for coc
 fi
 
 # Setup neovim
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -P $HOME/pkgs/.
-chmod u+x $HOME/pkgs/nvim.appimage
-sudo mv $HOME/pkgs/nvim.appimage /usr/bin/nvim.appimage
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+mv nvim-linux.tar.gz $HOME/pkgs/.
+cd $HOME/pkgs
+tar xvf  nvim-linux64.tar.gz
+chmod u+x $HOME/pkgs/nvim-linux64/bin/nvim
 sudo rm /usr/bin/vim
-sudo ln -s /usr/bin/nvim.appimage /usr/bin/vim
+sudo ln -s /home/ubuntu/pkgs/nvim-linux64/bin/nvim /usr/bin/vim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
-git clone https://github.com/penn-microbioinfo/microbioinfo-aws.git $HOME/microbioinfo-aws
+#git clone https://github.com/penn-microbioinfo/microbioinfo-aws.git $HOME/microbioinfo-aws
 mkdir -p $HOME/.config
-ln -s $PATH_TO_MBIAWS/setup_instance/config/nvim $HOME/.config/.
+ln -s $PATH_TO_MBIAWS/microbioinfo-aws/setup_instance/config/nvim $HOME/.config/.
