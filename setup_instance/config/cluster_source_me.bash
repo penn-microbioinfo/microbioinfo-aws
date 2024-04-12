83368c0160fe85189a9c27b6936a69a11b9fdb3f
AWS ParallelCluster user <ubuntu@ip-10-0-4-37.ec2.internal>
2024-04-12 23:38:45 +0000

    s

--- setup_instance/config/cluster_source_me.bash
+++ setup_instance/config/cluster_source_me.bash
@@ -1,6 +1,6 @@
 CLUSTHOME=/shared-ebs
 MBIAWS=$CLUSTHOME/microbioinfo-aws
-export PATH=/$CLUSTHOME/bin:/$CLUSTHOME/.local/bin:$PATH
+export PATH=/$CLUSTHOME/bin:/$CLUSTHOME/.local/bin:/opt/slurm/bin:$PATH
 export PATH=$PATH:/$CLUSTHOME/build/bowtie2-2.5.2-linux-x86_64
 export XDG_CONFIG_HOME=$CLUSTHOME/.config
 export XDG_DATA_HOME=$CLUSTHOME/.local/share
