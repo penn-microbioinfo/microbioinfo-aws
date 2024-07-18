2c5402af3457ba7e2a052ed964dd666b8b1eb3d5
amsesk <amsesk@upenn.edu>
2024-07-18 18:45:33 +0000

    ups

--- setup_instance/current/install_python312.bash
+++ setup_instance/current/install_python312.bash
@@ -14,7 +14,7 @@ wget https://www.python.org/ftp/python/3.12.1/Python-3.12.1.tgz
 tar xvfz Python-3.12.1.tgz
 rm Python-3.12.1.tgz
 cd Python-3.12.1
-./configure --prefix=$PREFIX --enable-optimizations --with-openssl=/usr
+./configure --prefix=$PREFIX --enable-optimizations --with-openssl=/usr --enable-loadable-sqlite-extensions=yes
 make
 sudo make altinstall 
 sudo rm -rf $BUILD_DIR/Python-3.12.1
