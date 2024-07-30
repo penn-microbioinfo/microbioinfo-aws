1bfdd208988d679346347b9d01286362d9c983d5
AWS ParallelCluster user <ubuntu@ip-10-0-3-60.ec2.internal>
2024-07-30 17:17:27 +0000

    add paths

--- setup_instance/config/cluster_source_me.bash
+++ setup_instance/config/cluster_source_me.bash
@@ -4,5 +4,7 @@ export PATH=/$CLUSTHOME/bin:/$CLUSTHOME/.local/bin:/opt/slurm/bin:$PATH
 export PATH=$PATH:/$CLUSTHOME/build/bowtie2-2.5.2-linux-x86_64
 export XDG_CONFIG_HOME=$CLUSTHOME/.config
 export XDG_DATA_HOME=$CLUSTHOME/.local/share
+export POETRY_VIRTUALENVS_IN_PROJECT=true
 source $MBIAWS/setup_instance/config/zshrc
+export PATH=/home/ubuntu/.local/bin:$PATH
 cd $CLUSTHOME
