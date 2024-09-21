5da37f00cec546b246e60d0dd32ff87e67804b1d
amsesk <amsesk@upenn.edu>
2024-09-21 15:59:16 +0000

    use binary install only

--- setup_instance/current/setup_go.sh
+++ setup_instance/current/setup_go.sh
@@ -3,6 +3,6 @@
 GO_VERSION=go1.23.1
 osudo apt-get install bison
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-gvm install ${GO_VERSION}
+gvm install ${GO_VERSION} -B
 gvm use ${GO_VERSION} --default
 
